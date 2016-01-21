02bb709223eab0e7a305af885ac6f2c415f10fa1
doraoam <doraoam2005@gmail.com>
2016-01-21 02:46:25 +0200

    test

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    private const string roomName = "Board";
+    private RoomInfo[] roomsList;
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+            {
+                PhotonNetwork.CreateRoom(roomName + System.Guid.NewGuid().ToString("N"), new RoomOptions(){maxPlayers = 2}, null);
+            }
+
+            if (roomsList != null)
+            {
+                for (int i = 0; i < roomsList.Length; i++)
+                {
+                    if ((GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name)))
+                    {
+                        PhotonNetwork.JoinRoom(roomsList[i].name);
+                    }
+                }
+            }
+        }
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+
+    public GameObject playerPrefab;
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int score = 0;
+
+    public int enemyScore = 0;
+
+    ShapesManager main;
+
+    void Awake()
+    {
+        main = GameObject.FindGameObjectWithTag("shapesmanager").GetComponent<ShapesManager>();
+    }
+
+    public void setScore()
+    {
+        score = main.getScore();        
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(score);
+        }
+        else
+        {
+            enemyScore = (int)stream.ReceiveNext();   
+        }
+    }
+}
